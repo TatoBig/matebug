a343f7784499c52a4fbd49e298b5ec9cf774c5ba
MarianoGudi <49624930+Marianito5382@users.noreply.github.com>
2023-06-11 00:16:08 -0600

    identify classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,28 @@
 
 </script>
 
-<h1 class="w-10 h-20 text- bg-red-500">
-  Landing page
+<h1  class="w-90 h-34 bg-red-500 text-center">
+  MATE
   <Header />
 </h1>
+
+<main class="mx-96 my-20 bg-yellow-200">
+
+  <div>
+    <span class= "text-7xl text-justify">
+      <br> Bienvenidos a la pagina de MATEBUG, donde te enseñaremos metodos los cuaales,. son de aprendizaje
+      facil y dinamico
+    </span>
+  </div>
+  <div>
+    
+  </div>
+  
+
+</main>
+
+<style>
+  .text-primary{
+    color: blue;
+  }
+</style>
