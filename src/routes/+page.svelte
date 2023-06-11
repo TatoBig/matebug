639fc004c46612f1477358af708c56653b584604
MarianoGudi <49624930+Marianito5382@users.noreply.github.com>
2023-06-11 13:15:49 -0600

    f

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,50 @@
 <script>
-	import Header from "@components/layout/Header.svelte";
+	import { Path } from "$env/static/private";
+import Header from "@components/layout/Header.svelte";
+	import { group_outros } from "svelte/internal";
 
 </script>
 
-<h1  class="w-90 h-34 bg-red-500 text-center">
-  MATE
-  <button>
+<header  class=" container bg-red-500 mx-auto px-4 py-6 flex
+items-center justify-between">
+  <a href="/" class="font-bold text-white text xl"> pipe </a> 
+  <nav></nav>
+  <nav>
+    <ul>
+      <li>
+        <a href="#" class = "rounded-full px-3 py-2 front semibold bg-white
+         bg-opacity-10 flex items-center group ">
+         <span class="mr-2">Comenzemos</span>
 
-  </button>
-  <Header />
+        </a> 
+      </li>
+    </ul>
+  </nav>
+
   
-</h1>
+</header>
 
-<main class="mx-96 my-20 bg-orange-400 text-center ">
+<main class="mx-96 my-20 bg-gradient-to-br from-orange-300 to-orange-700 via-orange-500 
+antialiase bg-no-repeat ">
 
   <div>
-    <span class= "text-8xl font-bold ">
+    <span class= "text-7xl font-bold ">
       MATEBUG
     </span>
     <p class=" text-3xl text-justify">
       <br >
       "Descubre el fascinante mundo de las matemáticas básicas: tu guía 
       confiable para comprender, aprender y disfrutar de los fundamentos numéricos que rigen nuestro universo."
+      <br>
+      Comenzemos con temas basicos, lo cuales podemos agregar soluciones rapidas y efectivas pára que podamos realizar 
+      comprension.
     </p>
-    <img src="static/learn-math.jpg" style="width: 300px; height: auto;" />
-    <p>
-
-    </p>
+    <h4 class="my-20 font-bold">Estamos contratando gente que sea capaz y se arriesgue al cambioi </h4>
 
   </div>
   
 
 </main>
 
-<style>
 
-</style>
+
