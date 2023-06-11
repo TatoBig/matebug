8ec34ae60649915b1d9e4c20bd87e0961c13a260
MarianoGudi <49624930+Marianito5382@users.noreply.github.com>
2023-06-11 01:49:38 -0600

    Settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,38 @@
 <script>
 	import Header from "@components/layout/Header.svelte";
 
-
 </script>
 
 <h1  class="w-90 h-34 bg-red-500 text-center">
   MATE
+  <button>
+
+  </button>
   <Header />
+  
 </h1>
 
-<main class="mx-96 my-20 bg-yellow-200">
+<main class="mx-96 my-20 bg-orange-400 text-center ">
 
   <div>
-    <span class= "text-7xl text-justify">
-      <br> Bienvenidos a la pagina de MATEBUG, donde te enseñaremos metodos los cuaales,. son de aprendizaje
-      facil y dinamico
+    <span class= "text-8xl font-bold ">
+      MATEBUG
     </span>
-  </div>
-  <div>
-    
+    <p class=" text-3xl text-justify">
+      <br >
+      "Descubre el fascinante mundo de las matemáticas básicas: tu guía 
+      confiable para comprender, aprender y disfrutar de los fundamentos numéricos que rigen nuestro universo."
+    </p>
+    <img src="static/learn-math.jpg" style="width: 300px; height: auto;" />
+    <p>
+
+    </p>
+
   </div>
   
 
 </main>
 
 <style>
-  .text-primary{
-    color: blue;
-  }
+
 </style>
