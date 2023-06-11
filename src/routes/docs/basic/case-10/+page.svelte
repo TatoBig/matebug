e8d93e6deb1660ebfcbc48d26cdb1647e3bfa84c
Guerrasdavid77 <dguerra680@gmail.com>
2023-06-11 16:55:49 -0600

    finised all factorization cases

---
+++ src/routes/docs/basic/case-10/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+	import Divider from '@components/core/Divider.svelte';
+	import Formula from '@components/core/Formula.svelte';
+</script>
+
+<div class="h-screen 2xl:mx-[600px] xl:mx-96 md:mx-40 p-16">
+	<h1>CONCEPTOS DE FACTORIZACIÓN</h1>
+
+	<Divider />
+	<h2>1. DIFERENCIA DE DOS CUADRADOS:</h2>
+	<p>
+		Sucede cuando tienes una expresión de la forma a⁴ - b⁴, que se factoriza utilizando la identidad
+		algebraica correspondiente.
+	</p>
+	<h4>Forma algebraica:</h4>
+	<p>
+		La diferencia de dos cuadrados se presenta en la forma de una expresión donde se resta el
+		cuadrado de un término del cuadrado de otro término. Por ejemplo, a^2 - b^2.
+	</p>
+	<h4>Términos cuadrados:</h4>
+	<p>
+		En la diferencia de dos cuadrados, hay dos términos cuadrados, uno correspondiente al primer
+		término y otro correspondiente al segundo término. Por ejemplo, en a^2 - b^2, los términos
+		cuadrados son a^2 y b^2.
+	</p>
+	<h4>Signo:</h4>
+	<p>
+		La diferencia de dos cuadrados se caracteriza por tener un signo de resta (-) entre los dos
+		términos. Esto indica que se está restando el cuadrado de un término del cuadrado de otro
+		término.
+	</p>
+	<h4>Propiedad de la factorización:</h4>
+	<p>
+		La diferencia de dos cuadrados se puede factorizar utilizando la fórmula de diferencia de
+		cuadrados, que es (a + b)(a - b). Esta fórmula permite simplificar y factorizar la expresión.
+	</p>
+	<h4>Aplicaciones:</h4>
+	<p>
+		La diferencia de dos cuadrados se utiliza en álgebra para simplificar expresiones y factorizar
+		polinomios. También es útil en áreas como el cálculo y la geometría, donde se pueden encontrar
+		diferencias de cuadrados en problemas de áreas o volúmenes.
+	</p>
+	<h4>Relación con identidades algebraicas:</h4>
+	<p>
+		La diferencia de dos cuadrados está relacionada con la identidad algebraica conocida como la
+		identidad de diferencia de cuadrados. Esta identidad establece cómo factorizar y simplificar la
+		diferencia de dos cuadrados utilizando la fórmula mencionada anteriormente.
+	</p>
+
+	<h4 class="font-normal">EJEMPLO</h4>
+	<Formula math={'(x² + 4)(x + 2)(x - 2)= 0'} />
+	<h4 class="font-normal">SOLUCION</h4>
+	<Formula math={' (x² + 4)(x² - 4)=0'} />
+	<Formula math={'x⁴ - 16=0'} />
+
+	<Divider />
+	<h2>2. CARACTERISTICAS DE DIFERENCIA DE DOS CUADRADOS</h2>
+	<p>
+		La diferencia de dos cuadrados es una expresión algebraica en la forma a^2 - b^2, donde se resta
+		el cuadrado de un término del cuadrado de otro término. Se puede factorizar utilizando la
+		fórmula de diferencia de cuadrados, y su factorización permite simplificar y factorizar la
+		expresión. La diferencia de dos cuadrados se aplica en álgebra y otras áreas de las matemáticas,
+		y está relacionada con la identidad de diferencia de cuadrados.
+	</p>
+
+	<h4 class="font-normal">Ejercicio 1</h4>
+	<Formula math={'(2y + 3z)(2y - 3z)(4y² - 9z²) =  0'} />
+	<!-- Esta es la solucion -->
+	<h4>Solución</h4>
+	<Formula math={' (4y² + 9z²)(4y² - 9z²)=0'} />
+	<Formula math={' 16y⁴ - 81z⁴=0'} />
+	<!-- Aqui termine -->
+
+	<h4 class="font-normal">Ejercicio 2</h4>
+	<Formula math={'(√5a⁴ + 2√2b⁴)(√5a⁴ - 2√2b⁴)= 0'} />
+	<!-- Esta es la solucion -->
+	<h4>Solución</h4>
+	<Formula math={' (5a⁴ + 8b⁴)(5a⁴ - 8b⁴)=0'} />
+	<Formula math={'25a⁸ - 64b⁸=0'} />
+	<!-- Aqui termine -->
+</div>
---
+++ src/routes/docs/basic/case-7/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+	import Divider from '@components/core/Divider.svelte';
+	import Formula from '@components/core/Formula.svelte';
+</script>
+
+<div class="h-screen 2xl:mx-[600px] xl:mx-96 md:mx-40 p-16">
+	<h1>CONCEPTOS DE FACTORIZACIÓN</h1>
+
+	<Divider />
+	<h2>1. DIFERENCIA DE CUBOS:</h2>
+	<p>
+		Se aplica cuando tienes una expresión de la forma a³ - b³, que se factoriza como (a - b)(a² + ab
+		+ b²).
+	</p>
+	<h4>Forma algebraica:</h4>
+	<p>
+		Una diferencia de cubos se presenta en la forma de una resta entre dos términos que son cubos
+		perfectos. La forma general es a^3 - b^3.
+	</p>
+	<h4>Factorización:</h4>
+	<p>
+		Una diferencia de cubos puede ser factorizada utilizando la fórmula para la factorización de
+		diferencia de cubos, que es (a - b)(a^2 + ab + b^2).
+	</p>
+	<h4>Términos cúbicos:</h4>
+	<p>
+		En una diferencia de cubos, tanto el primer término (a^3) como el segundo término (b^3) son
+		cubos perfectos.
+	</p>
+	<h4>Signo:</h4>
+	<p>
+		La diferencia de cubos se caracteriza por tener un signo negativo (-) entre los dos términos.
+		Esto indica que se está restando un cubo del otro.
+	</p>
+	<h4>Propiedad de la factorización:</h4>
+	<p>
+		La factorización de una diferencia de cubos es única y se puede utilizar para simplificar la
+		expresión y reducir su complejidad.
+	</p>
+	<h4>Aplicaciones:</h4>
+	<p>
+		Las diferencias de cubos son útiles en matemáticas y ciencias para simplificar expresiones
+		algebraicas y resolver ecuaciones cúbicas. También pueden ser utilizadas en el cálculo de
+		volúmenes y áreas en problemas de geometría.
+	</p>
+
+	<h4 class="font-normal">EJEMPLO</h4>
+	<Formula math={'(2x - 3)(4x² + 6x + 9) = 0'} />
+	<h4 class="font-normal">SOLUCION</h4>
+	<Formula math={'8x³ - 27=0'} />
+
+	<Divider />
+	<h2>2. CARACTERISTICAS DIFERENCIA DE CUBOS</h2>
+	<p>
+		Una diferencia de cubos es una expresión algebraica en la forma a^3 - b^3, donde ambos términos
+		son cubos perfectos. Se puede factorizar utilizando la fórmula de diferencia de cubos, y su
+		factorización es única. Las diferencias de cubos son útiles para simplificar expresiones y
+		resolver ecuaciones cúbicas en diversas aplicaciones matemáticas y científicas.
+	</p>
+
+	<h4 class="font-normal">Ejercicio 1</h4>
+	<Formula math={'(3y - 4)(9y² + 12y + 16) =  0'} />
+	<!-- Esta es la solucion -->
+	<h4>Solución</h4>
+	<Formula math={'27y³ - 64=0'} />
+	<!-- Aqui termine -->
+
+	<h4 class="font-normal">Ejercicio 2</h4>
+	<Formula math={' (5a - 4b)(25a² + 20ab + 16b²) = 0'} />
+	<!-- Esta es la solucion -->
+	<h4>Solución</h4>
+	<Formula math={'  125a³ - 64b³ =0'} />
+	<!-- Aqui termine -->
+</div>
